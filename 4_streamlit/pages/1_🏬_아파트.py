4ef67d94bc813dbc6bef61821daff41834ba4441
ghdaud30 <ghdaud3093@gmail.com>
2023-12-18 15:55:11 +0900

    Streamlit Update

--- 4_streamlit/pages/1_🏬_아파트.py
+++ 4_streamlit/pages/1_🏬_아파트.py
@@ -135,7 +135,6 @@ st.sidebar.markdown(
     - [오피스텔](https://www.data.go.kr/data/15059249/openapi.do)
     - [학교(나이스)](https://open.neis.go.kr/portal/data/service/selectServicePage.do?page=1&rows=10&sortColumn=&sortDirection=&infId=OPEN17020190531110010104913&infSeq=2)
     - [지역별 공원](https://www.data.go.kr/data/15012890/standard.do)
-    - [지역별 인구(kosis 공유서비스)](https://kosis.kr/statHtml/statHtml.do?orgId=101&tblId=DT_1B040A3&vw_cd=MT_ZTITLE&list_id=A_7&scrId=&seqNo=&lang_mode=ko&obj_var_id=&itm_id=&conn_path=MT_ZTITLE&path=%252FstatisticsList%252FstatisticsListIndex.do)
     - [지리 정보 수집(지오서비스)](http://www.gisdeveloper.co.kr/?p=2332)
 """
 )
